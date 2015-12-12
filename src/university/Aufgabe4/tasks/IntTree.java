9bf50328458cab4e0d4453819b5802579ca3282c
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-12-12 18:45:09 +0100

    finished task 4

--- src/university/Aufgabe4/tasks/IntTree.java
+++ src/university/Aufgabe4/tasks/IntTree.java
@@ -12,21 +12,46 @@ class IntTree {
 
         void add(int elem) {
             //TODO add code here
+            if (elem < this.elem) {
+                if (left == null) {
+                    left = new Node(elem);
+                } else {
+                    left.add(elem);
+                }
+            } else if (elem > this.elem) {
+                if (right == null) {
+                    right = new Node(elem);
+                } else {
+                    right.add(elem);
+                }
+            }
         }
 
         int countNodes() {
             //TODO add code here
-            return 0;
+            return 1 + (this.right == null ? 0 : this.right.countNodes()) + (this.left == null ? 0 : this.left.countNodes());
         }
 
         int countLeaves() {
             //TODO add code here
-            return 0;
+            if (this.right == null && this.left == null) {
+                return 1;
+            }
+            return (this.left == null ? 0 : this.left.countLeaves()) + (this.right == null ? 0 : this.right.countLeaves());
         }
 
         int height() {
             //TODO add code here
-            return 0;
+            int leftHeight = 0;
+            int rightHeight = 0;
+            if (this.left != null) {
+                leftHeight = this.left.height();
+            }
+            if (this.right != null) {
+                rightHeight = this.right.height();
+            }
+
+            return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
         }
     }
 
@@ -34,6 +59,11 @@ class IntTree {
 
     public void add(int elem) {
         //TODO add code here
+        if (this.root == null) {
+            this.root = new Node(elem);
+        } else {
+            this.root.add(elem);
+        }
     }
 
     public boolean empty() {
@@ -42,16 +72,16 @@ class IntTree {
 
     public int countNodes() {
         //TODO add code here
-        return 0;
+        return this.empty() ? 0 : this.root.countNodes();
     }
 
     public int countLeaves() {
         //TODO add code here
-        return 0;
+        return this.empty() ? 0 : this.root.countLeaves();
     }
 
     public int height() {
         //TODO add code here
-        return 0;
+        return this.empty() ? 0 : this.root.height();
     }
 }
